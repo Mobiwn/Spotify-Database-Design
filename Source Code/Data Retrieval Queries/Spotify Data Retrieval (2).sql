e09faa8f509fe8cb97215fd4cece625a77b12b49
Muhammad Ibrahim <mibrahimpro.1@gmail.com>
2025-12-16 05:00:21 +0500

    Update Spotify Data Retrieval (2).sql
    Fixed the Join Bug

--- Source Code/Data Retrieval Queries/Spotify Data Retrieval (2).sql
+++ Source Code/Data Retrieval Queries/Spotify Data Retrieval (2).sql
@@ -1,5 +1,6 @@
 SELECT t.Track_ID, t.Name AS Track_Name, a.Name AS Artist_Name, l.Like_Date_Time
 FROM Likes l
 JOIN Tracks t ON l.Track_ID = t.Track_ID
-JOIN Artists a ON a.artist_id = a.Artist_ID
+JOIN Albums al ON t.Album_ID = al.Album_ID
+JOIN Artists a ON al.Artist_ID = a.Artist_ID
 WHERE l.User_ID = 1;
